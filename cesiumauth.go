b1a8526affaf2958ca176781ee3cd49075cd3e67
GaoMjun <jun2675731483@gmail.com>
2022-04-25 16:17:52 +0800

    no message

---
+++ cesiumauth.go
@@ -0,0 +1,84 @@
+package cesiumauth
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+
+	"go.uber.org/zap"
+)
+
+func init() {
+	caddy.RegisterModule(CesiumAuth{})
+}
+
+type CesiumAuth struct {
+	logger *zap.Logger
+
+	Url string `json:"url"`
+
+	authorization string
+}
+
+func (CesiumAuth) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.cesium_auth",
+		New: func() caddy.Module { return new(CesiumAuth) },
+	}
+}
+
+func (self *CesiumAuth) Provision(ctx caddy.Context) (err error) {
+	self.logger = ctx.Logger(self)
+
+	go func() {
+		self.updateAuthorization(self.Url)
+
+		for {
+			<-time.After(time.Second * 60)
+			self.updateAuthorization(self.Url)
+		}
+	}()
+	return
+}
+
+func (self CesiumAuth) Validate() error {
+	return nil
+}
+
+func (self *CesiumAuth) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) (err error) {
+	r.Header.Set("Authorization", "Bearer "+self.authorization)
+
+	err = next.ServeHTTP(w, r)
+	return
+}
+
+func (self *CesiumAuth) updateAuthorization(u string) {
+	var (
+		err  error
+		resp *http.Response
+		body []byte
+		ep   = endpoint{}
+	)
+
+	if resp, err = http.Get(u); err != nil {
+		return
+	}
+
+	if body, err = ioutil.ReadAll(resp.Body); err != nil {
+		return
+	}
+
+	if err = json.Unmarshal(body, &ep); err != nil {
+		return
+	}
+
+	self.authorization = ep.AccessToken
+}
+
+type endpoint struct {
+	AccessToken string `json:"accessToken"`
+}
